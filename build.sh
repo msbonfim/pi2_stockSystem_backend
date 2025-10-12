73e72db5156a54e68772c3e106f80c9203a790e7
msbonfim <msbonfim@yahoo.com.br>
2025-10-12 16:48:00 -0300

    quinto commit - backend

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ set -o errexit
 pip install -r requirements.txt
 
 # Executar migrações
-python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Coletar arquivos estáticos
+python manage.py collectstatic --no-input
 
 # Criar superusuário se não existir (opcional)
 # python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
--- sistema_gestao/settings.py
+++ sistema_gestao/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Para servir arquivos estáticos
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Configuração do WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
