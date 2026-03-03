b56c23d112979f340d2306ec1db644d6dd37448f
Marcel de Souza Bonfim <88121489+msbonfim@users.noreply.github.com>
2026-03-02 22:02:12 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -53,4 +53,4 @@ else
 fi
 
 # Criar superusuário se não existir (opcional)
-# python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u, c = User.objects.get_or_create(username='admin'); u.set_password('SuaNovaSenha123!'); u.is_superuser=True; u.is_staff=True; u.save()"
