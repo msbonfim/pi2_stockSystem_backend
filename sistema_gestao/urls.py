a33a22181dd9ac53849e4a11b0943412ab4d6309
Marcel de Souza Bonfim <88121489+msbonfim@users.noreply.github.com>
2026-03-02 21:57:36 -0300

    Update urls.py

--- sistema_gestao/urls.py
+++ sistema_gestao/urls.py
@@ -21,22 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.http import JsonResponse
-# --- INÍCIO DO CÓDIGO TEMPORÁRIO ---
-def resetar_senha_admin(request):
-    # Busca o usuário 'admin'. Se ele não existir (banco resetado), cria um novo.
-    user, created = User.objects.get_or_create(username='admin')
-    
-    # Define a nova senha (troque pela senha que você quer usar)
-    user.set_password('SuaNovaSenha123!') 
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
-    
-    if created:
-        return HttpResponse("Superusuário 'admin' CRIADO. Por favor, apague este código do GitHub agora!")
-    else:
-        return HttpResponse("Senha do 'admin' ALTERADA com sucesso. Por favor, apague este código do GitHub agora!")
-# --- FIM DO CÓDIGO TEMPORÁRIO ---
+
 def home(request):
     return JsonResponse({
         'message': 'Sistema de Gestão de Estoque - API Backend',
@@ -53,8 +38,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    # Adicione esta linha com uma URL difícil de adivinhar
-    path('rota-secreta-recuperar-senha/', resetar_senha_admin),
+
 ]
 
 # Servir arquivos estáticos e de mídia em ambiente de desenvolvimento
